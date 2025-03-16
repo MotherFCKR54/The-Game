1c26f0f911492dcc54bfb047b196552b3082ff4a
blint500 <124351711+MotherFCKR54@users.noreply.github.com>
2025-03-16 21:57:26 +0100

    Update

--- the-game/scenes/beallitas.gd
+++ the-game/scenes/beallitas.gd
@@ -1,17 +1,38 @@
 extends TextureButton
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 
 func _on_pressed() -> void:
+	$"../AudioStreamPlayer".play()
+
+
+func _on_nyelv_pressed() -> void:
+	$"../AudioStreamPlayer".play()
+
+
+func _on_stáblista_pressed() -> void:
+	$"../AudioStreamPlayer".play()
+	
+
+
+
+func _on_mouse_entered() -> void:
+	$"../AudioStreamPlayer2".play()
+
+
+func _on_nyelv_mouse_entered() -> void:
+	$"../AudioStreamPlayer2".play()
+
+
+func _on_stáblista_mouse_entered() -> void:
+	$"../AudioStreamPlayer2".play()
+
+
+func _on_v_issza_pressed() -> void:
 	$"../AudioStreamPlayer".play()
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/menü.tscn")
 
 
-func _on_mouse_entered() -> void:
+func _on_v_issza_mouse_entered() -> void:
 	$"../AudioStreamPlayer2".play()
--- the-game/scenes/start.gd
+++ the-game/scenes/start.gd
@@ -8,12 +8,27 @@ func _on_beállítások_pressed() -> void:
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/beállítások.tscn")
 func _on_beállítások_mouse_entered() -> void:
-	$"../egerravitel".play(1)
-func _on_credits_mouse_entered() -> void:
-	$"../egerravitel".play(1)
+	$"../egerravitel".play()
 func _on_mouse_entered() -> void:
 	$"../egerravitel".play()
-func _on_credits_pressed() -> void:
+
+
+func _on_folytatás_pressed() -> void:
 	$"../kattintas".play()
 	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://scenes/credits.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	
+
+func _on_kilépés_pressed() -> void:
+	$"../kattintas".play()
+	await get_tree().create_timer(1).timeout
+	get_tree().quit()
+
+
+func _on_folytatás_mouse_entered() -> void:
+	$"../egerravitel".play()
+
+
+
+func _on_kilépés_mouse_entered() -> void:
+	$"../egerravitel".play()
