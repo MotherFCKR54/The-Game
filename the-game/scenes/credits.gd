2e7328edda69a8a0be6fe8e5cffdad171f0defa5
kontorarmand <Kontorarmand@gmail.com>
2025-03-17 21:24:26 +0100

    MINDEN MEGVAN FASZOSOK GECCC

---
+++ the-game/scenes/credits.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+func _on_pressed() -> void:
+	$"../AudioStreamPlayer".play()
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://scenes/menü.tscn")
+
+func _on_mouse_entered() -> void:
+	$"../AudioStreamPlayer2".play()
