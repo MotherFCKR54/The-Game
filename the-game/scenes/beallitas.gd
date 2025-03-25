150cbf4a7af55e41a3282afc7c97546948131bab
kontorarmand <Kontorarmand@gmail.com>
2025-03-25 22:51:11 +0100

    Hangok menü fix
    Már csak jól kell kinéznie

--- the-game/scenes/beallitas.gd
+++ the-game/scenes/beallitas.gd
@@ -28,17 +28,6 @@ func _on_nyelv_mouse_entered() -> void:
 func _on_stáblista_mouse_entered() -> void:
 	$"../AudioStreamPlayer2".play()
 
-
-func _on_v_issza_pressed() -> void:
-	$"../AudioStreamPlayer".play()
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://scenes/menü.tscn")
-
-
-func _on_v_issza_mouse_entered() -> void:
-	$"../AudioStreamPlayer2".play()
-
-
 func _on_hangok_pressed() -> void:
 	$"../AudioStreamPlayer".play()
 	await get_tree().create_timer(1).timeout
@@ -46,3 +35,19 @@ func _on_hangok_pressed() -> void:
 
 func _on_hangok_mouse_entered() -> void:
 	$"../AudioStreamPlayer2".play()
+	
+func _on_v_issza_pressed() -> void:
+	$"../AudioStreamPlayer".play()
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://scenes/menü.tscn")
+
+func _on_v_issza_mouse_entered() -> void:
+	$"../AudioStreamPlayer2".play()
+	$"../VIssza".scale *= 1.1
+	$"../VIssza".position.x -= 10
+	$"../VIssza".position.y -= 10
+
+func _on_mouse_exited() -> void:
+	$"../VIssza".scale /= 1.1
+	$"../VIssza".position.x += 10
+	$"../VIssza".position.y += 10
--- the-game/scenes/vissza.gd
+++ the-game/scenes/vissza.gd
@@ -3,15 +3,15 @@ extends TextureButton
 func _on_pressed() -> void:
 	$"../AudioStreamPlayer".play()
 	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://scenes/menü.tscn")
+	get_tree().change_scene_to_file("res://scenes/beállítások.tscn")
 
 func _on_mouse_entered() -> void:
 	$"../AudioStreamPlayer2".play()
 	scale *= 1.1
-	position.x -= 5
-	position.y -= 4
+	position.x -= 10
+	position.y -= 10
 
 func _on_mouse_exited() -> void:
 	scale /= 1.1
-	position.x += 5
-	position.y += 4
+	position.x += 10
+	position.y += 10
