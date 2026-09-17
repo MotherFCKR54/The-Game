f6d1f7b100570e06b9c9cab22e6fd6974b3615ed
blint500 <124351711+MotherFCKR54@users.noreply.github.com>
2026-09-17 21:03:22 +0200

    Fix+update
    Bekerült egy új háttér és ki lett szedve az ugrás illetve a mozgás finomítva lett!

--- the-game/scenes/player_controller.gd
+++ the-game/scenes/player_controller.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite = $AgentAnimator/AnimatedSprite2D
@@ -11,10 +11,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
